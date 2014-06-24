20647f73fd3b10177044d3ccbfb531a60440d2c8
jonasduever <jonas@duever.de>
2014-06-24 18:34:56 +0200

    1st version (Interface + Map)

---
+++ js/milexp.js
@@ -0,0 +1,65 @@
+var SETTINGS = {
+	minYear: 2001,
+	maxYear: 2012,
+	defaultYear: 2012,
+	defaultMinValue: 100000
+}
+
+var FILTERS = {
+	types: [],
+	minValue: SETTINGS.defaultMinValue,
+	year: SETTINGS.defaultYear
+};
+
+$(function() {
+	$('#yearsSlider').slider({
+		min: SETTINGS.minYear,
+		max: SETTINGS.maxYear,
+		value: SETTINGS.defaultYear
+	}).on('slide', function(ev) {
+		$('#currYear').val(ev.value);
+		//updatedFilters();
+		setYear(ev.value);
+	})
+	
+	$('#currYear').val(SETTINGS.defaultYear).on('change keyup', function() {
+		$('#yearsSlider').slider('setValue', $(this).val());
+		//updatedFilters();
+		setYear($(this).val());
+	});
+	
+	$('#filterType').multiselect({
+		buttonClass: 'btn btn-link', // btn-primary
+		includeSelectAllOption: true,
+		includeSelectAllDivider: true,
+		selectedClass: null,
+		numberDisplayed: 2,
+		onChange: function(element, checked) {
+			//updatedFilters();
+			return false;
+		}
+	});
+	
+	$('#filterMinValue').val(SETTINGS.defaultMinValue).on('change keyup', function() {
+		//updatedFilters();
+	});
+});
+
+
+
+/*function updatedFilters() {
+	if($('#filterType').val() == null) FILTERS.types = [];
+	else FILTERS.types = $.grep($('#filterType').val(), function(value) {
+		return value != 'multiselect-all';
+	});
+	
+	FILTERS.year = $('#currYear').val();
+	FILTERS.minValue = $('#filterMinValue').val();
+	
+	
+	var map = $('#map').empty();
+	
+	map.append('Types: ' + FILTERS.types + '<br />');
+	map.append('Minimal value: ' + FILTERS.minValue + '<br />');
+	map.append('Year: ' + FILTERS.year);
+}*/
